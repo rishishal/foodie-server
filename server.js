5df5de06eb3c5a3fc52108a34962151fd5d0b21e
rishishal <rishi.kumar1609@gmail.com>
2024-04-20 19:34:48 +0530

    updated in API

--- server.js
+++ server.js
@@ -78,16 +78,10 @@ app.get("/api/menu", async (req, res) => {
 
 // For suggestion API
 app.get("/api/suggestion", async (req, res) => {
-  const {
-    "page-type": page_type,
-    "complete-menu": complete_menu,
-    lat,
-    lng,
-    searchQuery,
-  } = req.query;
+  const { lat, lng, searchQuery } = req.query;
   console.log(req.query);
 
-  const url = `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=${lat}&lng=${lng}&str=${searchQuery}&trackingId=undefined`;
+  const url = `https://www.swiggy.com/dapi/restaurants/search/suggest?lat=${lat}&lng=${lng}&str=${searchQuery}`;
 
   await fetch(url, {
     headers: {
@@ -115,44 +109,33 @@ app.get("/api/suggestion", async (req, res) => {
 
 // For search Query API
 app.get("/api/search", async (req, res) => {
-  const {
-    "page-type": page_type,
-    "complete-menu": complete_menu,
-    lat,
-    lng,
-    query,
-  } = req.query;
-  console.log(req.query);
+  const { lat, lng, query } = req.query;
 
-  const url = `https://www.swiggy.com/dapi/restaurants/search/v3?lat=${lat}&lng=${lng}&str=${query}&trackingId=undefined&submitAction=SUGGESTION&queryUniqueId=bd52aa24-315d-fdcb-661a-6eba3e1ad819&metaData=%7B%22type%22%3A%22DISH%22%2C%22data%22%3A%7B%22vegIdentifier%22%3A%22VEG%22%2C%22cloudinaryId%22%3A%22dyhqamt6bjfjwm4pkbat%22%2C%22dishFamilyId%22%3A%22846627%22%2C%22dishFamilyIds%22%3A%5B%22846627%22%5D%7D%2C%22businessCategory%22%3A%22SWIGGY_FOOD%22%2C%22displayLabel%22%3A%22Dish%22%7D`;
+  const url = `https://www.swiggy.com/dapi/restaurants/search/v3?lat=${lat}&lng=${lng}&str=${query}&trackingId=undefined&submitAction=SUGGESTION`;
 
-  await fetch(url, {
-    headers: {
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    },
-  })
-    .then((response) => {
-      if (!response.ok) {
-        throw new Error("Network response was not ok");
-      }
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      res.json(data);
-    })
-    .catch((error) => {
-      console.error(error);
-      res.status(500).send("An error occurred");
+  try {
+    const response = await fetch(url, {
+      headers: {
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "User-Agent":
+          "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
+      },
     });
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const data = await response.json();
+    res.json(data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("An error occurred");
+  }
 });
 
 app.get("/", (req, res) => {
   res.json({
-    test: "Welcome to FoodFire! - See Live Web URL for this Server - https://foodfire-app.netlify.app",
+    test: "Welcome to Foodie - See Live - https://foodie-rishi.vercel.app/",
   });
 });
 
